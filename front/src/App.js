9fe3ef714316db4b10f8d8cfe386c257c5d6bc50
Renaudin Bryan <bryan.renaudin@epitech.eu>
2023-01-25 16:36:03 +0100

    Add: page first

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
+import Home from './component/home/home';
 
 function App() {
-  axios("https://www.betaseries.com/authorize"),{
-    client_id: "91a576165315"
-    redirect_uri: "https://localhost:3000/home"
-  }
+  <div>t'es sur app</div>
 }
 
-export default App;
+export default Home;
---
+++ front/src/component/home/home.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Table from "react-bootstrap/Table";
+export default function Home(){
+    const [data,setData] = useState()
+    useEffect(() => {
+    axios.get('https://api.betaseries.com/shows/member?client_id=8e5e3832e2c7&token=87c53f44167d')
+    .then((res)=>{ 
+        setData(res.data.shows)
+    })
+},[]);
+console.log(data)
+
+    function archiver(){
+        
+    }
+    return(
+        <div>
+        <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>Illustration</th>
+          <th>Titre</th>
+          <th>Next episode à regarder</th>
+          <th>Totaux</th>
+          <th>%</th>
+          <th>Archiver</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data !== null && data !== undefined ?
+        data.map((item)=>(
+        <tr>
+            <th><img src={item.images.box}></img></th>
+            <th>{item.original_title}</th>
+            <td>{item.user.next.code}</td>
+            <td>{item.seasons} saisons, {item.episodes} episodes</td>
+            <td>{item.user.status}%  | {item.user.remaining} ép. | {item.length*item.user.remaining} minutes</td>
+            <td onClick={archiver}>&#10007;</td>
+        </tr>
+        ))
+         :null
+        }
+        
+      </tbody>
+    </Table>
+    {data !== null && data !== undefined && data[0].user.archived === true ?
+    <Table striped bordered hover >
+    <thead>
+      <tr>
+        <th>Illustration</th>
+        <th>Titre</th>
+        <th>Next episode à regarder</th>
+        <th>Totaux</th>
+        <th>%</th>
+        <th>Archiver</th>
+      </tr>
+    </thead>
+    <tbody>
+      {data !== null && data !== undefined ?
+      data.map((item)=>(
+      <tr>
+          <th><img src={item.images.box}></img></th>
+          <th>{item.original_title}</th>
+          <td>{item.user.next.code}</td>
+          <td>{item.seasons} saisons, {item.episodes} episodes</td>
+          <td>{item.user.status}%  | {item.user.remaining} ép. |</td>
+          <td onClick={archiver} >&#10007;</td>
+      </tr>
+      ))
+       :null
+      }
+      
+    </tbody>
+  </Table>: null
+    }
+  </div>
+    )
+}
+
